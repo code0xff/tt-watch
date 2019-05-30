033d0c8883385d39af69d9a90a6aa2f56a5d5a27
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2019-05-30 21:57:59 +0900

    change displayed message

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ export default class App extends Component {
 
   _setTTTime = (ttTime) => {
     const remainingTime = ttTime.getTime() - new Date().getTime();
-    this.setState({time: 'ㅌㅌ까지 ' + (remainingTime >= 0 ? remainingTime : 0) + 'ms 남았습니다.'})
+    this.setState({time: (remainingTime >= 0 ? remainingTime : 0) + 'ms'})
   }
 
   render () {
